2c65e4519986c869bc1334a283b31823608e8339
KatyaBako <katebako939@gmail.com>
2022-03-31 18:27:30 +0300

    add problems.py

---
+++ problems.py
@@ -0,0 +1,16 @@
+# sizes = ["S", "M", "L"]
+# tpl = (1, 2, 3, sizes)
+# tpl [3][1] = "XL"
+# print(tpl)
+
+# tpl = ("qwe", "qw", 2, 3)
+# print(len(tpl))
+# tpl = tpl * 2
+# print(tpl)
+
+s1 = {1, 2, 3, 4}
+# print(s1)
+# s1.add("nanana")
+# print(s1)
+# s1.update(["la", "lo", "lu"])
+# print(s1)
